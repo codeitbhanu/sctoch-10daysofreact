39ae661e94fe3ecdd3ffe575e31f232c758402a9
codeitbhanu <codeitbhanu@gmail.com>
2021-01-18 00:05:34 +0200

    Completed Excercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ScotchInfoBar from "./ScotchInfoBar";
+import user from "./assets/data";
+import UserInfo from "./UserInfo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* Show user data here */}
+            <div className="user-deets">
+                <UserInfo user={user} />
+            </div>
+
+            <ScotchInfoBar />
+        </div>
+    );
 }
 
 export default App;
